0626873eafae0136a896d52a1e5d0da2ecdbccef
Chris Broski <chrisbroski@gmail.com>
2016-09-18 09:57:35 -0400

    better private functions, nav links

--- template_src/aerophane/www/inc/main.js
+++ template_src/aerophane/www/inc/main.js
@@ -5,26 +5,27 @@ var aero;
 function Utilities() {
     "use strict";
 
-    this.getEventTarget = function getEventTarget(e) {
+    function getEventTarget(e) {
         var targ;
         targ = e.target || e.srcElement;
         if (targ.nodeType === 3) { // defeat Safari bug
             targ = targ.parentNode;
         }
         return targ;
-    };
+    }
+    this.getEventTarget = getEventTarget;
 
-    this.touchclick = function touchclick(el, func, bubble) {
+    function touchclick(el, func, bubble) {
         bubble = !!bubble;
         if ("ontouchstart" in window || "onmsgesturechange" in window) {
             el.addEventListener("touchstart", func, bubble);
         } else {
             el.addEventListener("click", func, bubble);
         }
-    };
+    }
+    this.touchclick = touchclick;
 
-    this.buildNav = function buildNav(navItems) {
-        "use strict";
+    function buildNav(navItems) {
         var navMatte, navNav, navH2, navP, navA;
 
         navMatte = document.createElement("div");
@@ -39,24 +40,36 @@ function Utilities() {
         navItems.forEach(function (item) {
             navP = document.createElement("p");
             navA = document.createElement("a");
-            // TODO: assign link
-            navA.textContent = item;
+            navA.textContent = item.name;
+            navA.href = item.href;
             navP.appendChild(navA);
             navNav.appendChild(navP);
         });
 
         document.body.appendChild(navNav);
 
-        this.touchclick(document.getElementById("matte"), function () {
+        touchclick(document.getElementById("matte"), function () {
             document.querySelector("nav").style.width = "0";
             document.getElementById("matte").style.display = "none";
         });
 
-        this.touchclick(document.querySelector("header button:first-child"), function () {
+        touchclick(document.querySelector("header button:first-child"), function () {
             document.querySelector("nav").style.width = "240px";
             document.getElementById("matte").style.display = "block";
         });
-    };
+    }
+    this.buildNav = buildNav;
+
+    /*this.scriptifyLinks = function scriptifyLinks() {
+        var allLinks = document.querySelector("a");
+        [].forEach.call(allLinks, function (el) {
+            touchclick(el, function (e) {
+                var
+                e.preventDefault();
+                location = this.getEventTarget
+            });
+        });
+    };*/
 }
 
 (function init() {
@@ -72,9 +85,15 @@ function Utilities() {
         window.history.back();
     });
 
-    aero.buildNav(['Home', 'List', 'Form']);
-    
+    aero.buildNav([
+        {"name": "Home", "href": "index.html"},
+        //{"name": "Add From Camera", "href": "camera.html"},
+        {"name": "List", "href": "list.html"},
+        {"name": "Form", "href": "form.html"}
+    ]);
+
     // document.addEventListener("deviceready", onDeviceReady);
 
     // Make links use JavaScript to keep iOS web apps from breaking out of the app window
+
 }());
