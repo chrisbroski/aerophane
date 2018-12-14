115d2ffd01e115349438ebaa9a06b23fc5469395
Chris Broski <chrisbroski@gmail.com>
2018-12-13 21:02:30 -0500

    example with not main nav

---
+++ template_src/www/nomenu/nomenu.js
@@ -0,0 +1,15 @@
+/*jslint browser: true */
+/*global Aerophane */
+
+var aero;
+
+(function init() {
+    "use strict";
+
+    if (aero) {
+        return;
+    }
+
+    aero = new Aerophane("Aerophane", null);
+
+}());
