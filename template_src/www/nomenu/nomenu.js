66b845ba74af13be21ac6ef384eaa412b8d4b367
Chris Broski <cbroski@eeiglobal.com>
2018-12-18 17:25:55 -0500

    spinners

--- template_src/www/nomenu/nomenu.js
+++ template_src/www/nomenu/nomenu.js
@@ -4,3 +4,15 @@ var backButton = document.querySelector("body > header > button:first-child");
 aero.touchclick(backButton, function () {
     history.back();
 });
+
+var prog = 0;
+function advanceProgress() {
+    var progHeader = document.querySelector(".progress-determinate");
+    progHeader.style = "--width:" + prog + "%";
+    prog += 0.3;
+    if (prog >= 100) {
+        prog = 0;
+    }
+    window.setTimeout(advanceProgress, 20);
+}
+advanceProgress();
